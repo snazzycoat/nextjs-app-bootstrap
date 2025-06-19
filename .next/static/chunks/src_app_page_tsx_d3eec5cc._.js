08722ca78cafd1183a6f6e7bd405366149f027da
snazzycoat <47192730+snazzycoat@users.noreply.github.com>
2025-06-19 20:36:04 +0200

    Add .next/static/chunks/src_app_page_tsx_d3eec5cc._.js 1750358164391

---
+++ .next/static/chunks/src_app_page_tsx_d3eec5cc._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_MapNoSSR_tsx_b8633664._.js",
+  "static/chunks/src_8c3a487a._.js",
+  "static/chunks/node_modules_49adce32._.js"
+],
+    source: "dynamic"
+});
