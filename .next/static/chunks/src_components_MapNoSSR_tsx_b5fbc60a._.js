d5d134062e5c7df3928a42e3c0504df3058e9dfc
snazzycoat <47192730+snazzycoat@users.noreply.github.com>
2025-06-19 20:36:17 +0200

    Add .next/static/chunks/src_components_MapNoSSR_tsx_b5fbc60a._.js 1750358176726

---
+++ .next/static/chunks/src_components_MapNoSSR_tsx_b5fbc60a._.js
@@ -0,0 +1,23 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/components/MapNoSSR.tsx [app-client] (ecmascript, next/dynamic entry, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "static/chunks/node_modules_b6fa4529._.js",
+  "static/chunks/src_components_MapNoSSR_tsx_5e15c1f1._.js",
+  {
+    "path": "static/chunks/node_modules_leaflet_dist_leaflet_88e19fd3.css",
+    "included": [
+      "[project]/node_modules/leaflet/dist/leaflet.css [app-client] (css)"
+    ]
+  },
+  "static/chunks/src_components_MapNoSSR_tsx_0f5a8e27._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/components/MapNoSSR.tsx [app-client] (ecmascript, next/dynamic entry)");
+    });
+});
+}}),
+}]);
